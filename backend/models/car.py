bb6fa209e39b00f71ccc177a8e328521c706119a
ahmat1 <rahmatmahamatdout@gmail.com>
2025-02-14 15:15:15 +0100

    ajooute picture in car models

--- backend/models/car.py
+++ backend/models/car.py
@@ -1,14 +1,14 @@
-
-from . import db
+from backend.models import db # Assurez-vous que db est bien importé depuis votre package
 
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(255), nullable=False)
     year = db.Column(db.Integer)
     model = db.Column(db.String(255))
     brand = db.Column(db.String(255))
     km = db.Column(db.Integer)
     type = db.Column(db.String(50))
     code = db.Column(db.String(50))
-    availability = db.Column(db.Boolean)
+    availability = db.Column(db.Boolean, default=True)
     description = db.Column(db.Text)
+    picture = db.Column(db.String(255))  # Chemin de l'image
