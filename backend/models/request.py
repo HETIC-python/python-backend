b11304dd83b83a38350fa11a0e2975827286c963
Faithgg <fglindayi@gmail.com>
2025-02-18 18:21:41 +0100

    cc

--- backend/models/request.py
+++ backend/models/request.py
@@ -4,6 +4,7 @@ from datetime import datetime
 class Request(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.Enum('rent', 'buy', 'trade-in'), nullable=False)
+    status = db.Column(db.Enum('new', 'pending', 'on-processing','done'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
     start_date = db.Column(db.Date, nullable=False)
