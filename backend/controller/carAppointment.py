3b3c03c8326e30d64da1a5b9aa025adb19f491f4
Hierarque <cieplucha.hugo@gmail.com>
2025-02-18 14:07:07 +0100

    typo fix

--- backend/controller/carAppointment.py
+++ backend/controller/carAppointment.py
@@ -2,22 +2,22 @@ from typing import Optional, List
 from models.carAppointment import CarAppointment, db
 from datetime import date
 
-def create_car_appointment(user_id: int, car_id: int, date: date) -> carAppointment:
+def create_car_appointment(user_id: int, car_id: int, date: date) -> CarAppointment:
     carAppointment = CarAppointment(user_id=user_id, car_id=car_id, date=date)
     db.session.add(carAppointment)
     db.session.commit()
     return carAppointment
 
-def get_car_appointment(appointment_id: int) -> Optional[carAppointment]:
+def get_car_appointment(appointment_id: int) -> Optional[CarAppointment]:
     return carAppointment.query.get(appointment_id)
 
-def get_car_appointments_by_user_id(user_id: int) -> List[carAppointment]:
+def get_car_appointments_by_user_id(user_id: int) -> List[CarAppointment]:
     return carAppointment.query.filter_by(user_id=user_id).all()
 
-def get_all_car_appointments() -> List[carAppointment]:
+def get_all_car_appointments() -> List[CarAppointment]:
     return carAppointment.query.all()
 
-def update_car_appointment(appointment_id: int, date: Optional[date]= None) -> Optional[carAppointment]:
+def update_car_appointment(appointment_id: int, date: Optional[date]= None) -> Optional[CarAppointment]:
     carAppointment = get_car_appointment(appointment_id)
     if carAppointment and date is not None :
         carAppointment.date = date
