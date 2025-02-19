7bc620797c7421296a469b1679599707ad454c9c
Hierarque <cieplucha.hugo@gmail.com>
2025-02-19 10:13:48 +0100

    changed date type to datetime for appointments

--- backend/controller/carAppointment.py
+++ backend/controller/carAppointment.py
@@ -1,8 +1,8 @@
 from typing import Optional, List
 from models.carAppointment import CarAppointment, db
-from datetime import date
+from datetime import datetime
 
-def create_car_appointment(user_id: int, car_id: int, date: date) -> CarAppointment:
+def create_car_appointment(user_id: int, car_id: int, date: datetime) -> CarAppointment:
     carAppointment = CarAppointment(user_id=user_id, car_id=car_id, date=date)
     db.session.add(carAppointment)
     db.session.commit()
@@ -17,7 +17,7 @@ def get_car_appointments_by_user_id(user_id: int) -> List[CarAppointment]:
 def get_all_car_appointments() -> List[CarAppointment]:
     return CarAppointment.query.all()
 
-def update_car_appointment(appointment_id: int, date: Optional[date]= None) -> Optional[CarAppointment]:
+def update_car_appointment(appointment_id: int, date: Optional[datetime]= None) -> Optional[CarAppointment]:
     carAppointment = get_car_appointment(appointment_id)
     if carAppointment and date is not None :
         carAppointment.date = date
